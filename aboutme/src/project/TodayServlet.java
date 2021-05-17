2ec9c4a1daaaee65f86821296d59233872733eed
JaneKangOfficial <rkdto89@naver.com>
2021-05-17 15:41:10 +0900

    first commit

---
+++ aboutme/src/project/TodayServlet.java
@@ -0,0 +1,39 @@
+package project;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/today")
+public class TodayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public TodayServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		Date date_now = new Date(System.currentTimeMillis());
+		SimpleDateFormat date_format = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+		
+		out.print("<html>");
+		out.print("<head>");
+		out.print("<a href=index.html>메인화면</a><br>");
+		out.print("</head>");
+		out.print("<body>");
+		out.print("<p>현재시간 : " + date_format.format(date_now)+"</p>");
+		out.print("</body>");
+		out.print("</html>");
+		
+	}
+}
